5e23ca6b20eb97f64a2dbca5350a8fa33a4c50ff
Chris <github@prettycode.org>
2024-07-24 09:42:48 -0700

    Placeholder for purpose of new project.

--- SyntheticCompositeEquityIndices/Program.cs
+++ SyntheticCompositeEquityIndices/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿throw new NotImplementedException("Combine SyntheticUsEquities and FundHistoryReturns output.");
