007dc92812f27a4850d3e7ade2665c1d16905d55
Chris <github@prettycode.org>
2024-07-24 12:09:02 -0700

    Updating fund ticker list with preliminary selections.

--- FundHistoryCache/Program.cs
+++ FundHistoryCache/Program.cs
@@ -5,17 +5,21 @@ static HashSet<string> GetFundTickers() => new HashSet<SortedSet<string>>([
         ["VFINX", "VOO"],       // US LCB
         ["DFLVX", "AVLV"],      // US LCV
         ["VIGAX"],              // US LCG
+        [/* ? */ "VSMAX"],      // US SCB
         ["DFSVX", "AVUV"],      // US SCV
         ["VSGAX"],              // US SCG
-        ["DFALX", "AVDE"],      // Int'l TSM
+        ["DFALX", "AVDE"],      // Int'l TSM/LCB
         ["DFIVX", "AVIV"],      // Int'l LCV
-        ["EFG"],                   // *Int'l LCG
+        [/* ? */ "EFG"],        // Int'l LCG
+        //["?"],                // Int'l SCB
         ["DISVX", "AVDV"],      // Int'l SCV
-        //["?"],                // *Int'l SCG
-        ["VEIEX", "AVEM"],      // EM
+        [/* ? */ "DISMX"],      // Int'l SCG
+        ["VEIEX", "AVEM"],      // EM TSM/LCB
         ["DFEVX", "AVES"],      // EM LCV
+        [/* ? */ "XSOE"],       // EM LCG
         ["DEMSX", "AVEE"],      // EM SCB
-        ["DGS"]                 // *EM SCV
+        ["DGS"],                // EM SCV
+        //["?"]                 // EM SCG
     ])
     .SelectMany(set => set).ToHashSet<string>();
 
