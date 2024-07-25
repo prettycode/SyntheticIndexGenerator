c9bb45ff60aa3cac182342305b1ec3dba0978f23
Chris <github@prettycode.org>
2024-07-24 22:15:02 -0700

    Refactor to spell out paths.

--- SyntheticCompositeEquityIndices/Program.cs
+++ SyntheticCompositeEquityIndices/Program.cs
@@ -1,12 +1,14 @@
 ﻿var rootPath = "../../../..";
-var fundRepository = new FundHistoryRepository($"{rootPath}/FundHistoryCache/data/");
+var fundRepositorySourcePath = Path.Combine(rootPath, "./FundHistoryCache/data/");
+var syntheticUsSourcePath = Path.Combine(rootPath, "./SyntheticUsEquityIndices/source/Stock-Index-Data-20220923-Monthly.csv");
+var syntheticUsSavePath = Path.Combine(rootPath, "./SyntheticUsEquityIndices/data/monthly/");
+var fundHistoryReturnsSavePath = Path.Combine(rootPath, "./FundHistoryReturns/data/");
+
+var fundRepository = new FundHistoryRepository(fundRepositorySourcePath);
 
 await Task.WhenAll(
-    SyntheticUsEquityIndicesController.SaveParsedReturnsToReturnsHistory(
-        $"{rootPath}/SyntheticUsEquityIndices/source/Stock-Index-Data-20220923-Monthly.csv",
-        $"{rootPath}/SyntheticUsEquityIndices/data/monthly/"
-    ),
+    SyntheticUsEquityIndicesController.SaveParsedReturnsToReturnsHistory(syntheticUsSourcePath, syntheticUsSavePath),
     FundHistoryCacheController.RefreshFundHistoryCache(fundRepository)
 );
 
-await FundHistoryReturnsController.WriteAllFundHistoryReturns(fundRepository, $"{rootPath}/FundHistoryReturns/data/");
+await FundHistoryReturnsController.WriteAllFundHistoryReturns(fundRepository, fundHistoryReturnsSavePath);
