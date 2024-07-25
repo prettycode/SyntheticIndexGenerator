b73dfe8eb5b23c5d231b331d320b7e144a79b32e
Chris <github@prettycode.org>
2024-07-24 22:09:41 -0700

    Simplify quotes.

--- SyntheticCompositeEquityIndices/Program.cs
+++ SyntheticCompositeEquityIndices/Program.cs
@@ -1,12 +1,12 @@
-﻿var rootPath = @"..\..\..\..";
-var fundRepository = new FundHistoryRepository(@$"{rootPath}\FundHistoryCache\data\");
+﻿var rootPath = "../../../..";
+var fundRepository = new FundHistoryRepository($"{rootPath}/FundHistoryCache/data/");
 
 await Task.WhenAll(
     SyntheticUsEquityIndicesController.SaveParsedReturnsToReturnsHistory(
-        @$"{rootPath}\SyntheticUsEquityIndices\source\Stock-Index-Data-20220923-Monthly.csv",
-        @$"{rootPath}\SyntheticUsEquityIndices\data\monthly\"
+        $"{rootPath}/SyntheticUsEquityIndices/source/Stock-Index-Data-20220923-Monthly.csv",
+        $"{rootPath}/SyntheticUsEquityIndices/data/monthly/"
     ),
     FundHistoryCacheController.RefreshFundHistoryCache(fundRepository)
 );
 
-await FundHistoryReturnsController.WriteAllFundHistoryReturns(fundRepository, @$"{rootPath}\FundHistoryReturns\data\");
+await FundHistoryReturnsController.WriteAllFundHistoryReturns(fundRepository, $"{rootPath}/FundHistoryReturns/data/");
