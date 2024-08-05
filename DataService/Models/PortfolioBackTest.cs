b050db9d59beca2a146090a8a9e697de0c58a0ac
Chris <github@prettycode.org>
2024-08-05 07:47:57 -0700

    Adding new model for anticipated final form of backtest.

---
+++ DataService/Models/PortfolioBackTest.cs
@@ -0,0 +1,23 @@
+﻿namespace DataService.Models
+{
+
+    public class PortfolioBackTest
+    {
+        public NominalPeriodReturn[] AggregatePerformance { get; set; }
+
+        public Dictionary<string, NominalPeriodReturn[]> DecomposedPerformanceByTicker { get; set; }
+
+        /// <summary>
+        /// Rebalances happen at the start of these.
+        /// </summary>
+        public DateTime[] Rebalanaces { get; set; }
+
+        public RebalanceStrategy RebalanceStrategy { get; set; }
+
+        /// <summary>
+        /// Scale is 0 - 100, not 0 - 1.
+        /// </summary>
+        public decimal? RebalanceThreshold { get; set; }
+
+    }
+}
