633749189e5f41cd5f102279802d052a88f13b1d
Chris <github@prettycode.org>
2024-08-06 06:32:15 -0700

    Minor optimizations to PortfolioBackTest definition.

--- DataService/Models/PortfolioBackTest.cs
+++ DataService/Models/PortfolioBackTest.cs
@@ -1,23 +1,21 @@
 ﻿namespace DataService.Models
 {
-
-    public class PortfolioBackTest
+    public struct PortfolioBackTest
     {
-        public NominalPeriodReturn[] AggregatePerformance { get; set; }
+        public required NominalPeriodReturn[] AggregatePerformance { get; set; }
 
-        public Dictionary<string, NominalPeriodReturn[]> DecomposedPerformanceByTicker { get; set; }
+        public required Dictionary<string, NominalPeriodReturn[]> DecomposedPerformanceByTicker { get; set; }
 
         /// <summary>
         /// List of of the periods, after completing, that triggered rebalances.
         /// </summary>
-        public DateTime[] CompletedPeriodsBeforeRebalances { get; set; }
+        public required DateTime[] CompletedPeriodsBeforeRebalances { get; set; }
 
-        public RebalanceStrategy RebalanceStrategy { get; set; }
+        public required RebalanceStrategy RebalanceStrategy { get; set; }
 
         /// <summary>
         /// Scale is 0 - 100, not 0 - 1.
         /// </summary>
-        public decimal? RebalanceThreshold { get; set; }
-
+        public required decimal? RebalanceThreshold { get; set; }
     }
 }
