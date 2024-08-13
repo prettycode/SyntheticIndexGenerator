c2cf7fbc7d6391efb8512e1da3f79db5e9fb9b18
Chris <github@prettycode.org>
2024-08-13 08:59:43 -0700

    Updating directory name for Quote tables.

--- Data/data/clean-quotes.ps1
+++ Data/data/clean-quotes.ps1
@@ -1,20 +1,14 @@
 ﻿$baseDir = "."
 
 $dirsToClean = @(
-    "$baseDir\quotes\QuoteDividend",
-    "$baseDir\quotes\QuotePrice",
-    "$baseDir\quotes\QuoteSplit"
+    "$baseDir\Quote\QuoteDividend",
+    "$baseDir\Quote\QuotePrice",
+    "$baseDir\Quote\QuoteSplit"
 )
 
 foreach ($dir in $dirsToClean) {
     if (Test-Path $dir) {
-        if ($dir -like "*\returns\*") {
-            # For returns directories, exclude files starting with "#"
-            Get-ChildItem $dir -File | Where-Object { $_.Name -notlike "#*" } | Remove-Item -Force
-        } else {
-            # For other directories, remove all files
-            Get-ChildItem $dir -File | Remove-Item -Force
-        }
+		Get-ChildItem $dir -File | Remove-Item -Force
         Write-Host "Cleaned $dir"
     } else {
         Write-Host "Directory not found: $dir"
--- Data/data/clean-returns.ps1
+++ Data/data/clean-returns.ps1
@@ -8,13 +8,8 @@ $dirsToClean = @(
 
 foreach ($dir in $dirsToClean) {
     if (Test-Path $dir) {
-        if ($dir -like "*\PeriodReturn\*") {
-            # For returns directories, exclude files starting with "#"
-            Get-ChildItem $dir -File | Where-Object { $_.Name -notlike "#*" } | Remove-Item -Force
-        } else {
-            # For other directories, remove all files
-            Get-ChildItem $dir -File | Remove-Item -Force
-        }
+		# Exclude files starting with "#"
+		Get-ChildItem $dir -File | Where-Object { $_.Name -notlike "#*" } | Remove-Item -Force
         Write-Host "Cleaned $dir"
     } else {
         Write-Host "Directory not found: $dir"
