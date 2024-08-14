650c30260efc0bc43983c6dc1559191e053b1937
Chris <github@prettycode.org>
2024-08-13 17:29:10 -0700

    Reorganizing in preparation for more refactoring.

--- Data/Returns/ReturnsService.cs
+++ Data/Returns/ReturnsService.cs
@@ -9,7 +9,10 @@ namespace Data.Returns
         private readonly IReturnRepository returnRepository;
         private readonly ILogger<ReturnsService> logger;
 
-        public ReturnsService(IQuotesService quotesService, IReturnRepository returnRepository, ILogger<ReturnsService> logger)
+        public ReturnsService(
+            IQuotesService quotesService,
+            IReturnRepository returnRepository,
+            ILogger<ReturnsService> logger)
         {
             ArgumentNullException.ThrowIfNull(nameof(quotesService));
             ArgumentNullException.ThrowIfNull(nameof(returnRepository));
@@ -20,9 +23,29 @@ namespace Data.Returns
             this.logger = logger;
         }
 
-        public async Task<Dictionary<string, Dictionary<PeriodType, PeriodReturn[]>>> GetReturns(
-            HashSet<string> tickers, bool skipRefresh = false)
+        public Task<List<PeriodReturn>> Get(string ticker, PeriodType period, DateTime startDate, DateTime endDate)
         {
+            return returnRepository.Get(ticker, period, startDate, endDate);
+        }
+
+        public async Task<Dictionary<string, Dictionary<PeriodType, PeriodReturn[]>>> GetReturns(
+            HashSet<string> tickers,
+            bool skipRefresh = false)
+        {
+            async Task<Dictionary<PeriodType, PeriodReturn[]>> ReturnsByPeriodType(
+                string ticker,
+                IEnumerable<QuotePrice> dailyPriceHistory)
+            {
+                ArgumentNullException.ThrowIfNull(ticker);
+                var periodTypes = Enum.GetValues<PeriodType>();
+
+                return await periodTypes
+                    .ToAsyncEnumerable()
+                    .SelectAwait(async periodType
+                        => (periodType, returns: await GetReturnsForPeriodType(ticker, dailyPriceHistory, periodType)))
+                    .ToDictionaryAsync(pair => pair.periodType, pair => pair.returns);
+            }
+
             ArgumentNullException.ThrowIfNull(tickers);
 
             var dailyPricesByTicker = await quotesService.GetPrices(tickers, skipRefresh);
@@ -31,28 +54,40 @@ namespace Data.Returns
                 .ToAsyncEnumerable()
                 .ToDictionaryAwaitAsync(
                     keySelector: pair => ValueTask.FromResult(pair.Key),
-                    elementSelector: async pair => await GetReturnsByPeriodType(pair.Key, pair.Value)
+                    elementSelector: async pair => await ReturnsByPeriodType(pair.Key, pair.Value)
                 );
         }
 
-        public Task<List<PeriodReturn>> Get(string ticker, PeriodType period, DateTime startDate, DateTime endDate)
+        private async Task<PeriodReturn[]> GetReturnsForPeriodType(
+            string ticker,
+            IEnumerable<QuotePrice> dailyPrices,
+            PeriodType periodType)
         {
-            return returnRepository.Get(ticker, period, startDate, endDate);
-        }
+            static List<PeriodReturn> GetPeriodOnlyReturns(List<PeriodReturn> returns, Func<DateTime, DateTime> adjustDate)
+                => returns.Select(r => r with { PeriodStart = adjustDate(r.PeriodStart) }).ToList();
 
-        private async Task<Dictionary<PeriodType, PeriodReturn[]>> GetReturnsByPeriodType(string ticker, IEnumerable<QuotePrice> dailyPriceHistory)
-        {
-            ArgumentNullException.ThrowIfNull(ticker);
-            var periodTypes = Enum.GetValues<PeriodType>();
+            static List<QuotePrice> GroupPricesByPeriod<TKey>(IEnumerable<QuotePrice> prices, Func<DateTime, TKey> keySelector)
+                => prices
+                    .GroupBy(r => keySelector(r.DateTime))
+                    .Select(g => g.OrderByDescending(r => r.DateTime).First())
+                    .OrderBy(r => r.DateTime)
+                    .ToList();
 
-            return await periodTypes
-                .ToAsyncEnumerable()
-                .SelectAwait(async periodType => (periodType, returns: await GetReturnsForPeriodType(ticker, dailyPriceHistory, periodType)))
-                .ToDictionaryAsync(pair => pair.periodType, pair => pair.returns);
-        }
+            static List<PeriodReturn> CalculateReturns(string ticker, IEnumerable<QuotePrice> prices, PeriodType periodType)
+            {
+                static decimal CalculateChange(decimal x, decimal y) => (y - x) / x * 100m;
 
-        private async Task<PeriodReturn[]> GetReturnsForPeriodType(string ticker, IEnumerable<QuotePrice> dailyPrices, PeriodType periodType)
-        {
+                return prices
+                    .Zip(prices.Skip(1), (prev, current) => new PeriodReturn
+                    {
+                        Ticker = ticker,
+                        PeriodStart = current.DateTime,
+                        ReturnPercentage = CalculateChange(prev.AdjustedClose, current.AdjustedClose),
+                        PeriodType = periodType
+                    })
+                    .ToList();
+            }
+
             var groupedPrices = periodType switch
             {
                 PeriodType.Daily => dailyPrices,
@@ -81,32 +116,6 @@ namespace Data.Returns
             }
 
             return [.. returnsForPeriodType];
-
-        }
-
-        private static List<PeriodReturn> GetPeriodOnlyReturns(List<PeriodReturn> returns, Func<DateTime, DateTime> adjustDate)
-            => returns.Select(r => r with { PeriodStart = adjustDate(r.PeriodStart) }).ToList();
-
-        private static List<QuotePrice> GroupPricesByPeriod<TKey>(IEnumerable<QuotePrice> prices, Func<DateTime, TKey> keySelector)
-            => prices
-                .GroupBy(r => keySelector(r.DateTime))
-                .Select(g => g.OrderByDescending(r => r.DateTime).First())
-                .OrderBy(r => r.DateTime)
-                .ToList();
-
-        private static List<PeriodReturn> CalculateReturns(string ticker, IEnumerable<QuotePrice> prices, PeriodType periodType)
-        {
-            static decimal CalculateChange(decimal x, decimal y) => (y - x) / x * 100m;
-
-            return prices
-                .Zip(prices.Skip(1), (prev, current) => new PeriodReturn
-                {
-                    Ticker = ticker,
-                    PeriodStart = current.DateTime,
-                    ReturnPercentage = CalculateChange(prev.AdjustedClose, current.AdjustedClose),
-                    PeriodType = periodType
-                })
-                .ToList();
         }
     }
 }
