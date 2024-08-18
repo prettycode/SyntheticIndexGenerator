88cf0649869779e499b9ec0ece85abc0212b93be
Chris <github@prettycode.org>
2024-08-18 10:47:51 -0700

    More clean-data scripts.

---
+++ Data/data/clean-returns-fake.ps1
@@ -0,0 +1,17 @@
+﻿$baseDir = "."
+
+$dirsToClean = @(
+    "$baseDir\PeriodReturn\Daily",
+    "$baseDir\PeriodReturn\Monthly",
+    "$baseDir\PeriodReturn\Yearly"
+)
+
+foreach ($dir in $dirsToClean) {
+    if (Test-Path $dir) {
+		# Exclude files starting with "#"
+		Get-ChildItem $dir -File | Where-Object { $_.Name -like "#*" } | Remove-Item -Force
+        Write-Host "Cleaned $dir"
+    } else {
+        Write-Host "Directory not found: $dir"
+    }
+}
