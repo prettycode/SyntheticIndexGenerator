a0e94b1324c6b213931aab1513b6a797fcc509b4
Chris <github@prettycode.org>
2024-08-22 15:53:46 -0700

    Cleanup.

--- TableFileCache/ThreadSafeFile.cs
+++ TableFileCache/ThreadSafeFile.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Concurrent;
 
 namespace TableFileCache;
+
 public static class ThreadSafeFile
 {
     private static readonly ConcurrentDictionary<string, SemaphoreSlim> fileLocks = new();
 
-    public static async Task<string[]> ReadAllLinesAsync(string path, CancellationToken cancellationToken = default)
+    private static async Task ExecuteWithFileLockAsync(
+        string path,
+        Func<string, CancellationToken, Task>
+        fileOperation,
+        CancellationToken cancellationToken = default)
     {
         var fileLock = fileLocks.GetOrAdd(path, _ => new SemaphoreSlim(1, 1));
 
-        await fileLock.WaitAsync();
+        await fileLock.WaitAsync(cancellationToken);
 
         try
         {
-            return await File.ReadAllLinesAsync(path, cancellationToken);
+            await fileOperation(path, cancellationToken);
         }
         finally
         {
@@ -26,18 +26,18 @@ public static class ThreadSafeFile
         }
     }
 
-    public static async Task AppendAllLinesAsync(
+    private static async Task<T> ExecuteWithFileLockAsync<T>(
         string path,
-        IEnumerable<string> contents,
+        Func<string, CancellationToken, Task<T>> fileOperation,
         CancellationToken cancellationToken = default)
     {
         var fileLock = fileLocks.GetOrAdd(path, _ => new SemaphoreSlim(1, 1));
 
-        await fileLock.WaitAsync();
+        await fileLock.WaitAsync(cancellationToken);
 
         try
         {
-            await File.AppendAllLinesAsync(path, contents, cancellationToken);
+            return await fileOperation(path, cancellationToken);
         }
         finally
         {
@@ -45,22 +45,26 @@ public static class ThreadSafeFile
         }
     }
 
-    public static async Task WriteAllLinesAsync(
+    public static Task<string[]> ReadAllLinesAsync(string path, CancellationToken cancellationToken = default)
+    {
+        return ExecuteWithFileLockAsync(path, File.ReadAllLinesAsync, cancellationToken);
+    }
+
+    public static Task AppendAllLinesAsync(
         string path,
         IEnumerable<string> contents,
         CancellationToken cancellationToken = default)
     {
-        var fileLock = fileLocks.GetOrAdd(path, _ => new SemaphoreSlim(1, 1));
+        return ExecuteWithFileLockAsync(path, (p, ct)
+            => File.AppendAllLinesAsync(p, contents, ct), cancellationToken);
+    }
 
-        await fileLock.WaitAsync();
-
-        try
-        {
-            await File.WriteAllLinesAsync(path, contents, cancellationToken);
-        }
-        finally
-        {
-            fileLock.Release();
-        }
+    public static Task WriteAllLinesAsync(
+        string path,
+        IEnumerable<string> contents,
+        CancellationToken cancellationToken = default)
+    {
+        return ExecuteWithFileLockAsync(path, (p, ct)
+            => File.WriteAllLinesAsync(p, contents, ct), cancellationToken);
     }
 }
