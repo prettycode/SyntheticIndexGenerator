5d0e85cbac606019476695e787744575a93a7f90
Chris <github@prettycode.org>
2024-09-11 07:31:29 -0700

    Cleaner pipeline.

--- pipeline-task.ps1
+++ pipeline-task.ps1
@@ -1,24 +1,20 @@
 param (
     [Parameter(Position=0, Mandatory=$true)]
-    [string]$Task,
-
+    [string]$Tasks,
     [Parameter(Position=1, Mandatory=$false)]
     [string]$ProjectOrSolution = "PortfolioAnalyzer.sln",
-
     [Parameter(Position=2, Mandatory=$false)]
     [string]$Configuration = "Debug"
 )
+
 function Execute-Command {
     param (
         [string]$Description,
         [string]$Command
     )
-
     Write-Host "$($Task): $($Description)..." -ForegroundColor Cyan
     Write-Host "> $Command"
-
     Invoke-Expression $Command
-
     if ($LASTEXITCODE -ne 0) {
         Write-Error "Command failed with exit code $LASTEXITCODE"
         exit 1
@@ -28,21 +24,22 @@ function Execute-Command {
 function Show-Usage {
     Write-Host @"
 Usage:
-.\pipeline-task.ps1 [Task] [SolutionFile] [Configuration]
-
-Task: clean, build, run, or test (required)
+.\pipeline-task.ps1 [Tasks] [SolutionFile] [Configuration]
+Tasks: Comma-separated list of tasks to execute (clean, build, test, run, run:WebApp.Server, run:WebApp.ClientLegacy, run:DataRefreshJob) (required)
 SolutionFile: Path to the solution file (optional, default: PortfolioAnalyzer.sln)
 Configuration: Debug or Release (optional, default: Debug)
-
 Examples:
-.\pipeline-task.ps1 build
-.\pipeline-task.ps1 build MySpecificSolution.sln Release
-.\pipeline-task.ps1 test
-.\pipeline-task.ps1 run PortfolioAnalyzer.sln Release
+.\pipeline-task.ps1 "run" MyProject.csproj
+.\pipeline-task.ps1 "build, test" MySpecificSolution.sln Release
+.\pipeline-task.ps1 "clean, build, test, run"
+.\pipeline-task.ps1 "run:WebApp.Server, run:WebApp.ClientLegacy"
 "@ -ForegroundColor Cyan
 }
 
-function Main {
+function Execute-Task {
+    param (
+        [string]$Task
+    )
     switch ($Task) {
         "clean" {
             Execute-Command "Cleaning project/solution" "dotnet clean $ProjectOrSolution --configuration $Configuration"
@@ -58,29 +55,31 @@ function Main {
             Execute-Command "Launching project" "dotnet run --project $ProjectOrSolution --configuration $Configuration --no-build"
         }
         "run:WebApp.Server" {
-            $Task = "run"
-            $ProjectOrSolution = "WebApp\\WebApp.Server\\WebApp.Server.csproj --launch-profile https"
-            Main
+            $ProjectOrSolution = "WebApp\WebApp.Server\WebApp.Server.csproj --launch-profile https"
+            Execute-Command "Launching WebApp.Server" "dotnet run --project $ProjectOrSolution --configuration $Configuration --no-build"
         }
         "run:WebApp.ClientLegacy" {
-            $Task = "run"
-            $ProjectOrSolution = "WebApp.ClientLegacy\\WebApp.ClientLegacy.esproj"
-            Main
+            $ProjectOrSolution = "WebApp.ClientLegacy\WebApp.ClientLegacy.esproj"
+            Execute-Command "Launching WebApp.ClientLegacy" "dotnet run --project $ProjectOrSolution --configuration $Configuration --no-build"
         }
         "run:DataRefreshJob" {
-            $Task = "run"
-            $ProjectOrSolution = "DataRefreshJob\\DataRefreshJob.csproj"
-            Main
+            $ProjectOrSolution = "DataRefreshJob\DataRefreshJob.csproj"
+            Execute-Command "Launching DataRefreshJob" "dotnet run --project $ProjectOrSolution --configuration $Configuration --no-build"
         }
         default {
-            Write-Warning "Unrecognized task `"$($Task)`"." -ForegroundColor Cyan
-            Show-Usage
-            exit 1
+            Write-Warning "Unrecognized task `"$($Task)`"." -ForegroundColor Yellow
         }
     }
 }
 
-if (-not $Task) {
+function Main {
+    $TaskList = $Tasks -split ',' | ForEach-Object { $_.Trim() }
+    foreach ($Task in $TaskList) {
+        Execute-Task $Task
+    }
+}
+
+if (-not $Tasks) {
     Show-Usage
 } else {
     Main
