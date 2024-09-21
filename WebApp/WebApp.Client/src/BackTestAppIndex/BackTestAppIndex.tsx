2d7f8de57ec5a91b591160514eba5791fcf41f55
Chris <github@prettycode.org>
2024-09-21 08:57:26 -0700

    Fix for CSS bleedover between front-end apps.

--- WebApp/WebApp.Client/src/BackTestAppIndex/BackTestAppIndex.tsx
+++ WebApp/WebApp.Client/src/BackTestAppIndex/BackTestAppIndex.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import BackTestApp from '../BackTestApp/BackTestApp';
 import './BackTestAppIndex.css';
 
-const BackTestAppIndex = () => <BackTestApp />;
-
-export default BackTestAppIndex;
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BackTestApp />
+    </React.StrictMode>
+);
--- WebApp/WebApp.Client/src/PortfolioAppIndex/PortfolioAppIndex.tsx
+++ WebApp/WebApp.Client/src/PortfolioAppIndex/PortfolioAppIndex.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import PortfolioApp from '../PortfolioApp/PortfolioApp';
 import './PortfolioAppIndex.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-const PortfolioAppIndex = () => <PortfolioApp />;
-
-export default PortfolioAppIndex;
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <PortfolioApp />
+    </React.StrictMode>
+);
